969eea6cde21d3748be11ea92752cbb41cca5030
Tyshchenko Dev <tyshchenkodev@gmail.com>
2025-04-14 11:55:25 +0200

    Create static component structure for Jammming UI

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchBar(props) {
+    return (
+        <div>
+            <input></input>
+            <button>Search</button>
+        </div>
+    )
+}
---
+++ src/components/Track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Track(props) {
+    return (
+        <div>
+            <h3>A Beautiful Lie</h3>
+            <span>30 Seconds to Mars</span>
+            <p>A Beautiful Lie</p>
+            <button>+</button>
+        </div>
+    )
+}

510f40bf1544e71c630b387ef364c7423458e7fe
Tyshchenko Dev <tyshchenkodev@gmail.com>
2025-04-14 11:11:48 +0200

    Create base components structure: SearchBar, TrackList, Track, Playlist, SearchResults

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,1 @@
-import React from "react";
-import TrackList from "./TrackList";
 
-export default function SearchResults(props) {
-    return(
-        <div>
-            <h2>Search Results</h2>
-            <TrackList tracks={props.tracks} onAdd={props.onAdd} />
-        </div>
-    )
-}
